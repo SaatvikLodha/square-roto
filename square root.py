a0a7d2143bdf929659172669a8766ad3215ad8e2
SaatvikLodha <saatviklodha@gmail.com>
2026-06-17 19:25:44 -0500

    Add files via upload

---
+++ square root.py
@@ -0,0 +1,5 @@
+#We are calculating for the square root of a number using Python.
+import math
+num = float(input("Please enter the number you would like to solve the square root of: "))
+print(math.sqrt(num),"is the square root.")
+
