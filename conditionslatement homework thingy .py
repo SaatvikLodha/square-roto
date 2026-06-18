9678f98c015582d06f63ec72adb471257ae45760
SaatvikLodha <saatviklodha@gmail.com>
2026-06-18 15:54:35 -0500

    Add files via upload

---
+++ conditionslatement homework thingy .py
@@ -0,0 +1,5 @@
+temperature=float(input("Please enter the temperature in Fahrenheit:"))
+if temperature>67.999999999999999999999999999999999999999:
+    print("It is suitable.")
+else:
+    print("It is not suitable.")
